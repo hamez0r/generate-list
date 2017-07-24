06bfb682b5ad9ea36fbae1b670c2089dc7b365cc
hame <andrei.epure90@gmail.com>
2017-07-24 10:36:44 +0300

    small refactoring

--- generate-list.js
+++ generate-list.js
@@ -13,25 +13,37 @@ let Quantity = function(str) {
   this.unit = tokens.length == 2 ? tokens[1] : ""
 }
 
+let ShoppingList = function() {
+  this.items = new Map() // "item" -> Quantity
+}
+
+ShoppingList.prototype = {
+  addItem: function(item, quantity) {
+    if (this.items.has(item)) 
+      this.items.get(item).value += quantity.value
+    else
+      this.items.set(item, quantity)
+  },
+
+  print: function() {
+    for (let [item, quantity] of this.items.entries()) {
+      let displayString = `${item}: ${quantity.value} ${quantity.unit}`
+      console.log(displayString)
+    }
+  }
+}
+
 let content = JSON.parse(fs.readFileSync('recipes.json'))
 let recipes = content.recipes
-let shoppingList = new Map() // "item" -> Quantity
+let shoppingList = new ShoppingList()
 
 for (let recipe of recipes) {
   for (let ingredient of recipe.ingredients) {
     let itemName = ingredient.name
     let itemQuantity = new Quantity(ingredient.quantity)
 
-    if (shoppingList.has(itemName)) {
-      let quantity = shoppingList.get(itemName)
-      quantity.value += itemQuantity.value
-    } else {
-      shoppingList.set(itemName, itemQuantity)
-    }
+    shoppingList.addItem(itemName, itemQuantity)
   }
 }
 
-for (let [item, quantity] of shoppingList.entries()) {
-  let displayString = `${item}: ${quantity.value} ${quantity.unit}`
-  console.log(displayString)
-}
+shoppingList.print()
